4e2db77ce40ba5ad591df42743a0e040e528c887
Tagan Hoyle <tagnum.elite@gmail.com>
2019-12-05 17:52:02 +0200

    Update makezip.bat

--- install/makezip.bat
+++ install/makezip.bat
@@ -2,23 +2,23 @@
 
 set DIR=%~dps0
 :: remove trailing backslash
-if "%DIR:~-1%" == "\" (
-    set DIR="%DIR:~0,-1%"
+if %DIR:~-1% == \ (
+    set DIR=%DIR:~0,-1%
 )
 
-set ROOT="%DIR%"\..
-set /p VERSION="%ROOT%"\io_scene_nif\VERSION
-set NAME="blender_nif_plugin"
-set PYFFI_VERSION="2.2.4.dev3"
-set DEPS="io_scene_nif\dependencies"
-if exist "%DIR%\temp" rmdir /s /q "%DIR%\temp"
+set ROOT=%DIR%\..
+set /p VERSION=< %ROOT%\io_scene_nif\VERSION
+set NAME=blender_nif_plugin
+set PYFFI_VERSION=2.2.4.dev3
+set DEPS=io_scene_nif\dependencies
+if exist %DIR%\temp rmdir /s /q %DIR%\temp
 
-mkdir "%DIR%"\temp
+mkdir %DIR%\temp
 
-pushd "%DIR%"\temp
+pushd %DIR%\temp
 mkdir io_scene_nif
-xcopy /s "%ROOT%"\io_scene_nif io_scene_nif
-mkdir "%DEPS%"
+xcopy /s %ROOT%\io_scene_nif io_scene_nif
+mkdir %DEPS%
 
 python -m pip install "PyFFI==%PYFFI_VERSION%" --target="%DEPS%"
 
