6fd64704f8be8e4984ca1d9d4283263604bfcb78
Tagan Hoyle <tagnum.elite@gmail.com>
2020-02-27 20:46:57 +0200

    Fix makezip.bat
    Add -v argument for more verbose xcopy

--- install/makezip.bat
+++ install/makezip.bat
@@ -3,29 +3,39 @@
 set DIR=%~dps0
 :: remove trailing backslash
 if "%DIR:~-1%" == "\" (
-    set DIR="%DIR:~0,-1%"
+    set DIR=%DIR:~0,-1%
 )
 
-set ROOT="%DIR%"\..
-set /p VERSION="%ROOT%"\io_scene_nif\VERSION
-set NAME="blender_nif_plugin"
-set PYFFI_VERSION="2.2.4.dev3"
+if "%~1"=="-v" (
+    set XCOPY_FLAG=/f
+) else (
+    set XCOPY_FLAG=/q
+)
+
+set ROOT=%DIR%\..
+set /p VERSION=<%ROOT%\io_scene_nif\VERSION
+set NAME=blender_nif_plugin
+set PYFFI_VERSION=2.2.4.dev3
 set DEPS="io_scene_nif\dependencies"
-if exist "%DIR%\temp" rmdir /s /q "%DIR%\temp"
+if exist %DIR%\temp rmdir /s /q %DIR%\temp
 
-mkdir "%DIR%"\temp
+echo NAME: %NAME%
+echo VERSION: %VERSION%
+echo PYFFI_VERSION: %PYFFI_VERSION%
 
-pushd "%DIR%"\temp
+mkdir %DIR%\temp
+
+pushd %DIR%\temp
 mkdir io_scene_nif
-xcopy /s "%ROOT%"\io_scene_nif io_scene_nif
-mkdir "%DEPS%"
+xcopy /s %XCOPY_FLAG% %ROOT%\io_scene_nif io_scene_nif
+mkdir %DEPS%
 
 python -m pip install "PyFFI==%PYFFI_VERSION%" --target="%DEPS%"
 
-xcopy "%ROOT%"\AUTHORS.rst io_scene_nif
-xcopy "%ROOT%"\CHANGELOG.rst io_scene_nif
-xcopy "%ROOT%"\LICENSE.rst io_scene_nif
-xcopy "%ROOT%"\README.rst io_scene_nif
+xcopy %XCOPY_FLAG% %ROOT%\AUTHORS.rst io_scene_nif
+xcopy %XCOPY_FLAG% %ROOT%\CHANGELOG.rst io_scene_nif
+xcopy %XCOPY_FLAG% %ROOT%\LICENSE.rst io_scene_nif
+xcopy %XCOPY_FLAG% %ROOT%\README.rst io_scene_nif
 popd
 
 powershell -executionpolicy bypass -Command "%DIR%\zip.ps1" -source "%DIR%\temp\io_scene_nif" -destination "%DIR%\%NAME%-%VERSION%.zip"
